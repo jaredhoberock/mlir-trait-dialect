0e4601c9cf35a67b83635edf6a6fb9a5de847938
Jared Hoberock <jaredhoberock@gmail.com>
2025-04-30 16:07:37 -0500

    Add C bindings for the trait dialect

--- cpp/c_api.cpp
+++ cpp/c_api.cpp
@@ -1,6 +1,7 @@
 #include "c_api.h"
 #include "Dialect.hpp"
 #include "Ops.hpp"
+#include "Types.hpp"
 #include <mlir/CAPI/IR.h>
 #include <mlir/CAPI/Pass.h>
 #include <mlir/IR/Builders.h>
@@ -12,6 +13,106 @@ extern "C" {
 
 void traitRegisterDialect(MlirContext context) {
   unwrap(context)->loadDialect<TraitDialect>();
+}
+
+MlirOperation traitTraitOpCreate(MlirLocation loc, MlirStringRef name) {
+  MLIRContext* ctx = unwrap(loc)->getContext();
+  OpBuilder builder(ctx);
+  auto op = builder.create<TraitOp>(
+    unwrap(loc),
+    builder.getStringAttr(StringRef(name.data, name.length))
+  );
+  return wrap(op.getOperation());
+}
+
+MlirOperation traitImplOpCreate(MlirLocation loc, MlirStringRef traitName, MlirType concreteType) {
+  MLIRContext* ctx = unwrap(loc)->getContext();
+  OpBuilder builder(ctx);
+  auto op = builder.create<ImplOp>(
+    unwrap(loc), 
+    FlatSymbolRefAttr::get(ctx, StringRef(traitName.data, traitName.length)),
+    TypeAttr::get(unwrap(concreteType))
+  );
+  return wrap(op.getOperation());
+}
+
+MlirOperation traitMethodCallOpCreate(MlirLocation loc,
+                                      MlirStringRef traitName, MlirType selfType,
+                                      MlirStringRef methodName,
+                                      MlirValue* arguments, intptr_t numArguments,
+                                      MlirType* resultTypes, intptr_t numResults) {
+  MLIRContext* ctx = unwrap(loc)->getContext();
+  OpBuilder builder(ctx);
+
+  SmallVector<Value> args;
+  for (intptr_t i = 0; i < numArguments; ++i) {
+    args.push_back(unwrap(arguments[i]));
+  }
+
+  SmallVector<Type> results;
+  for (intptr_t i = 0; i < numResults; ++i) {
+    results.push_back(unwrap(resultTypes[i]));
+  }
+
+  auto op = builder.create<MethodCallOp>(
+    unwrap(loc),
+    results,
+    FlatSymbolRefAttr::get(ctx, StringRef(traitName.data, traitName.length)),
+    TypeAttr::get(unwrap(selfType)),
+    FlatSymbolRefAttr::get(ctx, StringRef(methodName.data, methodName.length)),
+    args
+  );
+
+  return wrap(op.getOperation());
+}
+
+MlirOperation traitFuncCallOpCreate(MlirLocation loc,
+                                    MlirStringRef callee,
+                                    MlirValue* arguments, intptr_t numArguments,
+                                    MlirType* resultTypes, intptr_t numResults) {
+  MLIRContext* ctx = unwrap(loc)->getContext();
+  OpBuilder builder(ctx);
+
+  SmallVector<Value> args;
+  for (intptr_t i = 0; i < numArguments; ++i) {
+    args.push_back(unwrap(arguments[i]));
+  }
+
+  SmallVector<Type> results;
+  for (intptr_t i = 0; i < numResults; ++i) {
+    results.push_back(unwrap(resultTypes[i]));
+  }
+
+  auto op = builder.create<FuncCallOp>(
+    unwrap(loc),
+    results,
+    FlatSymbolRefAttr::get(ctx, StringRef(callee.data, callee.length)),
+    args
+  );
+
+  return wrap(op.getOperation());
+}
+
+MlirType traitSelfTypeGet(MlirContext ctx) {
+  return wrap(SelfType::get(unwrap(ctx)));
+}
+
+MlirType traitPolyTypeGet(MlirContext wrappedCtx,
+                          unsigned int uniqueId,
+                          MlirStringRef* traitBounds,
+                          intptr_t numTraitBounds) {
+  MLIRContext* ctx = unwrap(wrappedCtx);
+
+  // build an array of FlatSymbolRefAttr from the C strings
+  SmallVector<FlatSymbolRefAttr> bounds;
+  bounds.reserve(numTraitBounds);
+  for (intptr_t i = 0; i < numTraitBounds; ++i) {
+    auto &sref = traitBounds[i];
+    StringRef name(sref.data, sref.length);
+    bounds.push_back(FlatSymbolRefAttr::get(ctx, name));
+  }
+
+  return wrap(PolyType::get(ctx, uniqueId, bounds));
 }
 
 } // end extern "C"
--- cpp/c_api.h
+++ cpp/c_api.h
@@ -11,6 +11,33 @@ extern "C" {
 /// Manually register the trait dialect with a context.
 void traitRegisterDialect(MlirContext ctx);
 
+/// Create a trait.trait operation
+MlirOperation traitTraitOpCreate(MlirLocation loc, MlirStringRef name);
+
+/// Create a trait.impl operation
+MlirOperation traitImplOpCreate(MlirLocation loc, MlirStringRef traitName, MlirType concreteType);
+
+/// Create a trait.method.call operation
+MlirOperation traitMethodCallOpCreate(MlirLocation loc,
+                                      MlirStringRef traitName, MlirType selfType,
+                                      MlirStringRef methodName,
+                                      MlirValue* arguments, intptr_t numArguments,
+                                      MlirType* resultTypes, intptr_t numResults);
+
+/// Create a trait.func.call operation
+MlirOperation traitFuncCallOpCreate(MlirLocation loc,
+                                    MlirStringRef callee,
+                                    MlirValue* arguments, intptr_t numArguments,
+                                    MlirType* resultTypes, intptr_t numResults);
+
+/// Return the !trait.self type
+MlirType traitSelfTypeGet(MlirContext ctx);
+
+/// Return the !trait.poly<uniqueId, [@Trait1, @Trait2, ...]> type
+MlirType traitPolyTypeGet(MlirContext ctx,
+                          unsigned int uniqueId,
+                          MlirStringRef* traitBounds, intptr_t numTraitBounds);
+
 #ifdef __cplusplus
 }
 #endif
