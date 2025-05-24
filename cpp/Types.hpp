19ce9bf3d43a04ef80704101938c40e3f655dea7
Jared Hoberock <jaredhoberock@gmail.com>
2025-05-24 10:31:48 -0500

    Put forward declaration in the right place

--- cpp/Types.hpp
+++ cpp/Types.hpp
@@ -1,12 +1,17 @@
 #pragma once
 
 #include <mlir/IR/BuiltinTypes.h>
-#include "TypeInterfaces.hpp.inc"
 
 namespace mlir::trait {
 
 // forward declaration for SymbolicMatcherInterface
 class TraitOp;
+
+}
+
+#include "TypeInterfaces.hpp.inc"
+
+namespace mlir::trait {
 
 inline bool containsSymbolicType(Type ty) {
   // XXX TODO we actually need to traverse subelements of ty
